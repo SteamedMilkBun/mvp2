38bf41f8d9650c06f0f6022dc4eeda1a2145fbbe
Lance Tse <steam.bao666@gmail.com>
2024-02-01 12:09:04 -0800

    show chars on front end

--- public/app.js
+++ public/app.js
@@ -2,3 +2,25 @@ const $charContainer = $('#character-container');
 const $itemContainer = $('#item-container');
 const $optionsContainer = $('#options-container');
 const $charItemContainer = $('#char-item-container');
+const $listChar = $('#list-char');
+const $listItem = $('#list-item');
+
+const showAllChar = () => {
+    fetch("character")
+    .then((result) => {
+        result.json();
+    })
+    .then((chars) => {
+        for(let char of chars.rows) {
+            const $charDiv = $(`<div>${char.char_name}</div>`)
+                            .addclass('char-div');
+            $listChar.append($charDiv);
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+}
+
+showAllChar();

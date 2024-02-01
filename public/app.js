0d3a88e9ce09255a89cc9741e2c301cdcc44b643
Lance Tse <steam.bao666@gmail.com>
2024-02-01 12:15:45 -0800

    add logs to showAllChar to debug

--- public/app.js
+++ public/app.js
@@ -6,11 +6,13 @@ const $listChar = $('#list-char');
 const $listItem = $('#list-item');
 
 const showAllChar = () => {
+    console.log('fetch character');
     fetch("character")
     .then((result) => {
         result.json();
     })
     .then((chars) => {
+        console.log(chars.rows);
         for(let char of chars.rows) {
             const $charDiv = $(`<div>${char.char_name}</div>`)
                             .addclass('char-div');
